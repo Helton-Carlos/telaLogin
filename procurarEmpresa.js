933d684a44d53da8a45645cf578b9326d84769f3
Helton Carlos <heltoncarlospa@hotmail.com>
2022-01-01 18:21:04 -0300

    Criando localstorage

--- procurarEmpresa.js
+++ procurarEmpresa.js
@@ -1,11 +1,31 @@
 (function () {
-  fetch("https://jsonplaceholder.typicode.com/comments")
-    .then((response) => response.json())
-    .then((json) => {
-      for (let i = 0; i < json.length; i++) {
-        document.querySelector("#id").innerHTML = json[i].id;
-        document.querySelector("#email").innerHTML = json[i].email;
-        console.log( json[i].id);
+  document.querySelector("#bntEnviar").addEventListener("click", (e) => {
+    let empresa = document.querySelector("#empresa").value;
+    let nota = document.querySelector("#nota").value;
+    let status = document.querySelector("#status").value;
+    let id = 1;
+    if (!empresa || !nota || !status) {
+      alert("Preencha todos os campos*");
+    } else {
+      function getTabela(local) {
+        const row = document.createElement("tr");
+        row.innerHTML= `
+        <td>${local.id}</td>
+        <td>${local.nota}</td>
+        <td>${local.empresa}</td>
+        <td>${local.status}</td>
+        `
+        document.querySelector("#tabela").appendChild(row)
       }
-    });
+      const objEmpresa = { empresa, nota, status, id };
+      const local = localStorage.local ? JSON.parse(localStorage.local) : [];
+      local.push(objEmpresa);
+      localStorage.local = JSON.stringify(local);
+      document.querySelector("#empresa").value = "";
+      document.querySelector("#nota").value = "";
+      document.querySelector("#status").value = "";
+      getTabela(local)
+      window.location.href="index.html";
+    }
+  });
 })();
