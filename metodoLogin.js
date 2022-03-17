5da57eddaa2918b4f70016307dd6476f379a17d7
Helton <heltoncarlospa@hotmail.com>
2022-03-16 23:55:15 -0300

    ajuste local-token

--- metodoLogin.js
+++ metodoLogin.js
@@ -3,19 +3,19 @@
     let email = document.querySelector("#email").value;
     let senha = document.querySelector("#senha").value;
     console.log(email + senha);
-    if(email === "" || senha==="")
-    {
-        alert("Preencha corretamente os campos...")
-    }else{
-        if(email === "adm" || senha==="123456"){
-            window.location.href="index.html"
-        }else{
-            alert("Esse usuário não existe")
-        }
+    if (email === "" || senha === "") {
+      alert("Preencha corretamente os campos...");
+    } else {
+      if (email === "adm" || senha === "123456") {
+        let obj = {
+          email: email,
+          token: "dkaosdkaokdaosda",
+        };
+        localStorage.setItem("obj",  JSON.stringify(obj));
+        window.location.href = "index.html";
+      } else {
+        alert("Esse usuário não existe");
+      }
     }
   });
 })();
-
-
-
-
