2529ce04eb9477138925ec3105ba4a9ce778dd8c
Helton <heltoncarlospa@hotmail.com>
2022-03-19 16:17:00 -0300

    mudanca login-get

--- metodoLogin.js
+++ metodoLogin.js
@@ -2,20 +2,29 @@
   document.querySelector(".login").addEventListener("click", function () {
     let email = document.querySelector("#email").value;
     let senha = document.querySelector("#senha").value;
-    console.log(email + senha);
+
     if (email === "" || senha === "") {
       alert("Preencha corretamente os campos...");
     } else {
-      if (email === "adm" || senha === "123456") {
-        let obj = {
-          email: email,
-          token: "dkaosdkaokdaosda",
-        };
-        localStorage.setItem("obj",  JSON.stringify(obj));
-        window.location.href = "index.html";
-      } else {
-        alert("Esse usuário não existe");
-      }
+      if (email && senha) {
+        fetch("http://localhost:3000/api/usuarios")
+          .then((r) => r.json())
+          .then((usuarios) => {
+            const login = usuarios.result;
+            for (let i = 0; i < login.length; i++) {
+              if (login[i].email === email && login[i].senha === senha) {
+                let obj = {
+                  email: email,
+                  token: "sedsad978404sadd23xmnaz",
+                };
+                localStorage.setItem("obj", JSON.stringify(obj));
+                window.location.href = "index.html";
+              } else {
+                alert("Usuário ou senha não existente.");
+              }
+            }
+          });
+      } 
     }
   });
 })();
